e04d09245a929ea267db3bd6d45609f6fd195c68
Jeffrey <jeff.ho@hotmail.com>
2022-09-19 13:28:23 -0400

    Refactor code

--- questions.rb
+++ questions.rb
@@ -1,18 +1,18 @@
 class Question
   attr_accessor :question, :number1, :number2
-def initialize
+  def initialize
     @question = ""
     @number1 = rand(1..20)
     @number2 = rand(1..20)
-end
+  end
 
-def generate_question
-  @question = "What does #{@number1} plus #{@number2} equal?"
-end
+  def generate_question
+    @question = "What does #{@number1} plus #{@number2} equal?"
+  end
 
-def correct?(player_answer)
+  def correct?(player_answer)
    @number1 + @number2 == player_answer
-end
+  end
 end
 
 
